ded9bc6bf261900bd7e0955cccc590c06e5a08b9
zTrueAmir <amir.rodriguezm@konradlorenz.edu.co>
2024-08-25 23:49:22 -0500

    Material Angular
    Se incluye el angular material.

--- Sistema_Eps_Angular/src/app/app.module.ts
+++ Sistema_Eps_Angular/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatToolbarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
